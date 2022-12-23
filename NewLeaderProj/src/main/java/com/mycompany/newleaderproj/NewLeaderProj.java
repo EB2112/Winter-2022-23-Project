ec35d279ded177ce27a8dea3e7de4e7fe1ecd245
joshdutchik <77994722+jdutchik@users.noreply.github.com>
2022-12-23 17:37:54 -0600

    Ethan Smells

--- NewLeaderProj/src/main/java/com/mycompany/newleaderproj/NewLeaderProj.java
+++ NewLeaderProj/src/main/java/com/mycompany/newleaderproj/NewLeaderProj.java
@@ -13,5 +13,6 @@ public class NewLeaderProj {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Ethan!");
+        System.out.println("Hello Ethan x2!");
     }
 }
